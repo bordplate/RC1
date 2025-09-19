77152ca26082fbf7f6382d672afe8e18dd150a60
Vetle Hjelle <vetle@bordplate.no>
2025-09-19 17:05:53 +0200

    Decompiled main function and added/changed dependencies

---
+++ code/game/bmain.cpp
@@ -0,0 +1,3 @@
+#include "common.h"
+
+INCLUDE_ASM("code/_generated/nonmatchings/game/bmain", startLevel);
--- code/game/boot.cpp
+++ code/game/boot.cpp
@@ -1,3 +1,20 @@
 #include "common.h"
 
-INCLUDE_ASM("code/_generated/nonmatchings/game/boot", main);
+INCLUDE_ASM("code/_generated/nonmatchings/game/boot", ParseBin);
+
+typedef void (*StartLevelPtr)(void);
+
+extern "C" void FlushCache(int arg1);
+extern "C" void startLevel();
+extern "C" StartLevelPtr ParseBin();
+
+int main(int argc, char **argv) {
+    StartLevelPtr pcVar1 = startLevel;  // assign first function
+
+    while (true) {
+        pcVar1();             // call current function
+        pcVar1 = ParseBin();  // assign next function
+        FlushCache(0);
+        FlushCache(2);
+    }
+}
