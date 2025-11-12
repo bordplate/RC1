265a070b0e155c9d36e5de636ecb407e2fd2f92d
bordplate <vetle@bordplate.no>
2025-11-12 15:08:59 +0100

    Add more of game/framebuf.cpp

--- code/game/framebuf.cpp
+++ code/game/framebuf.cpp
@@ -3,7 +3,9 @@
 
 extern s64 D_152078 __attribute__((section(".data")));
 
-//INCLUDE_ASM("code/_generated/nonmatchings/game/framebuf", SetBackgroundColor_Fiii);
 void SetBackgroundColor(s32 r, s32 g, s32 b) {
     D_152078 = (long)r | ((long)g << 8) | ((long)b << 0x10) | (0x8000ULL << 0x10);
 }
+
+INCLUDE_ASM("code/_generated/nonmatchings/game/framebuf", PutDispBuffer__Fv);
+INCLUDE_ASM("code/_generated/nonmatchings/game/framebuf", PutDrawBufferLarge__Fv);
