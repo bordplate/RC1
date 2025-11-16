39a86b73ecc56c89310f855ec05f55016b289672
bordplate <vetle@bordplate.no>
2025-11-16 17:17:19 +0100

    Update names in initonce.cpp

--- code/game/initonce.cpp
+++ code/game/initonce.cpp
@@ -1,7 +1,7 @@
 #include "common.h"
 
-INCLUDE_ASM("code/_generated/nonmatchings/game/initonce", InitMemSlots);
+INCLUDE_ASM("code/_generated/nonmatchings/game/initonce", InitMemSlots__Fv);
 
-INCLUDE_ASM("code/_generated/nonmatchings/game/initonce", InitOnce);
+INCLUDE_ASM("code/_generated/nonmatchings/game/initonce", InitOnce__Fv);
 
 INCLUDE_ASM("code/_generated/nonmatchings/game/initonce", func_00201A20);
