861ed9a14126040a1b3b830f29c18140f0a7b4eb
isaka2025 <bouvetproj@gmail.com>
2026-04-08 13:55:49 +0200

    Split fonts.cpp, freeze.cpp and help.cpp

---
+++ code/game/freeze.cpp
@@ -0,0 +1,7 @@
+#include "common.h"
+
+INCLUDE_ASM("code/_generated/nonmatchings/game/freeze", mode_freezeInit);
+
+INCLUDE_ASM("code/_generated/nonmatchings/game/freeze", DrawDialogText__Fv);
+
+INCLUDE_ASM("code/_generated/nonmatchings/game/freeze", UpdateModeFreeze__Fv);
