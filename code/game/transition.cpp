f8d6db4ef49e5e3346702f7933ffd63762116c83
isaka2025 <bouvetproj@gmail.com>
2026-05-27 16:02:59 +0200

    Split actuator and transition

---
+++ code/game/transition.cpp
@@ -0,0 +1,21 @@
+#include "common.h"
+
+INCLUDE_ASM("code/_generated/nonmatchings/game/transition", Transition_DrawSky__Fv);
+
+INCLUDE_ASM("code/_generated/nonmatchings/game/transition", func_001E9B10);
+
+INCLUDE_ASM("code/_generated/nonmatchings/game/transition", Transition_LoadWad);
+
+INCLUDE_ASM("code/_generated/nonmatchings/game/transition", func_001EAF50);
+
+INCLUDE_ASM("code/_generated/nonmatchings/game/transition", Transition_UpdateMovieCamera__Fv);
+
+INCLUDE_ASM("code/_generated/nonmatchings/game/transition", Transition_FUN_001eb0a8);
+
+INCLUDE_ASM("code/_generated/nonmatchings/game/transition", Transition_DefaultDraw__Fb);
+
+INCLUDE_ASM("code/_generated/nonmatchings/game/transition", func_001EB740);
+
+INCLUDE_ASM("code/_generated/nonmatchings/game/transition", Transition_DoTransition__Fv);
+
+INCLUDE_ASM("code/_generated/nonmatchings/game/transition", func_001EBC88);
