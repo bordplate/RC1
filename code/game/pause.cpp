8e7a3023164353a74a317ff04d9dbaec7e7e0e6c
isaka2025 <bouvetproj@gmail.com>
2026-05-28 14:30:18 +0200

    .text is fully split now? yolo

--- code/game/pause.cpp
+++ code/game/pause.cpp
@@ -1,5 +1,7 @@
 #include "common.h"
 
+INCLUDE_ASM("code/_generated/nonmatchings/game/pause", func_00218D10);
+
 INCLUDE_ASM("code/_generated/nonmatchings/game/pause", PauseAllSounds);
 
 INCLUDE_ASM("code/_generated/nonmatchings/game/pause", func_00218F50);
---
+++ code/game/stash.cpp
@@ -0,0 +1,11 @@
+#include "common.h"
+
+INCLUDE_ASM("code/_generated/nonmatchings/game/stash", func_00232CE0);
+
+INCLUDE_ASM("code/_generated/nonmatchings/game/stash", func_00232D00);
+
+INCLUDE_ASM("code/_generated/nonmatchings/game/stash", Stash_SendData);
+
+INCLUDE_ASM("code/_generated/nonmatchings/game/stash", func_00232F20);
+
+INCLUDE_ASM("code/_generated/nonmatchings/game/stash", func_00233038);
